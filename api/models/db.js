74796b4b873fdc8399320c4e14eb757f33b96be4
Arthur Brant <arthurcl.br@hotmail.com>
2022-06-14 17:45:18 -0300

    use mysql2's async wrapper

--- api/models/db.js
+++ api/models/db.js
@@ -1,10 +1,16 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const connection = mysql.createConnection({
-    host: process.env.MARIADB_HOSTNAME,
-    user: process.env.MARIADB_USER,
-    password: process.env.MARIADB_PASSWORD,
-    database: process.env.MARIADB_DATABASE,
-});
+require('dotenv').config();
 
-exports.default = connection;
+async function query(query, values = '') {
+    const connection = await mysql.createConnection({
+        host: process.env.MARIADB_HOSTNAME,
+        user: process.env.MARIADB_USER,
+        password: process.env.MARIADB_PASSWORD,
+        database: process.env.MARIADB_DATABASE,
+    });
+    const [rows, fields] = await connection.execute(query, values);
+    return rows;
+}
+
+module.exports = query;
