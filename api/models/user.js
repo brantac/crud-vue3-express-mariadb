17a9f4dd45011069092a518e161d0e4731051fba
Arthur Brant <arthurcl.br@hotmail.com>
2022-06-14 20:02:43 -0300

    implement function to update user data

--- api/models/user.js
+++ api/models/user.js
@@ -1,18 +1,22 @@
 const db = require('./db');
 
 async function getAllUsers() {
-    let query = "SELECT * FROM user";
+    let query = 'SELECT * FROM `user`';
     let rows = await db(query);
     return rows;
 }
 
 async function getAllUsersAddress() {
-    let query = `
-    SELECT u.user_id, u.name, a.city
-    FROM user u LEFT JOIN address a ON u.address_id = a.address_id
-    `;
+    let query = '' +
+    'SELECT u.user_id, u.name, a.city ' +
+    'FROM user u LEFT JOIN address a ON u.address_id = a.address_id';
     let rows = await db(query);
     return rows;
+}
+
+async function updateUser(id, fields) {
+    let query = 'UPDATE `user` SET ? WHERE user_id = ?';
+    let rows = await db(query, [fields, id]);
 }
 
 module.exports = {
