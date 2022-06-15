a0fc07f5b2781a8daf9766f82b8ca186354fb050
Arthur Brant <arthurcl.br@hotmail.com>
2022-06-15 10:53:50 -0300

    get the email and password submitted

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,7 +1,13 @@
 async function signup (req, res) {
+    const {email, password} = req.body
+
+    console.log(email, password)
     res.send('user signed up')
 }
 async function login (req, res) {
+    const {email, password} = req.body
+
+    console.log(email, password)
     res.send('user logged in')
 }
 
