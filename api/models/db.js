177253adb4a3251112be5585d8f3ce863b413eeb
Arthur Brant <arthurcl.br@hotmail.com>
2022-06-15 09:49:04 -0300

    send an empty array if db connection fails

--- api/models/db.js
+++ api/models/db.js
@@ -3,14 +3,19 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 async function query(query, values = '') {
-    const connection = await mysql.createConnection({
-        host: process.env.MARIADB_HOSTNAME,
-        user: process.env.MARIADB_USER,
-        password: process.env.MARIADB_PASSWORD,
-        database: process.env.MARIADB_DATABASE,
-    });
-    const [rows, fields] = await connection.execute(query, values);
-    return rows;
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.MARIADB_HOSTNAME,
+            user: process.env.MARIADB_USER,
+            password: process.env.MARIADB_PASSWORD,
+            database: process.env.MARIADB_DATABASE,
+        });
+        const [rows, fields] = await connection.execute(query, values);
+        return rows;        
+    } catch (error) {
+        console.error("Error while trying to connect to the server: ");
+        return [];
+    }
 }
 
 module.exports = query;
