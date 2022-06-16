31cd360515e8454fb15e1449d5cb6199181256cf
Arthur Brant <arthurcl.br@hotmail.com>
2022-06-16 13:29:18 -0300

    add signup page route

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from './views/Home.vue';
 import Login from './views/Login.vue';
+import Signup from './views/Signup.vue';
 import User from './views/User.vue';
 import Users from './views/Users.vue';
 
@@ -9,6 +10,7 @@ const routes = [
     { path: '/user/:id', name: 'user', component: User },
     { path: '/users', component: Users },
     { path: '/login', component: Login },
+    { path: '/signup', component: Signup },
 ];
 
 const router = createRouter({
