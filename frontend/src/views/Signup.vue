28fe61fdeb5060818a3ef7dad411234fe57f29c1
Arthur Brant <arthurcl.br@hotmail.com>
2022-06-16 13:28:16 -0300

    create signup page

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,65 @@
+<template>
+    <main class="login-section">
+        <h1>Signup</h1>
+        <form
+            class="login-form"
+        >
+            <div class="input-group">
+                <label for="name">Name</label>
+                <input type="name" id="name" name="name" />
+            </div>
+            <div class="input-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" />
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" />
+            </div>
+            <button class="submit-login-btn" @click="" >
+                Create account    
+            </button>
+        </form>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.login-section {
+    padding: 0 1rem;
+}
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+}
+.input-group {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 1rem;
+}
+.input-group label {
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+.input-group input {
+    font-size: 1.2rem;
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+}
+.submit-login-btn {
+    background: #9e30b7;
+    color: white;
+    border: 1px solid white;
+    padding: 1rem 2rem;
+    border-radius: 0.25rem;
+    font-size: 1.2rem;
+    font-weight: bold;
+}
+</style>
